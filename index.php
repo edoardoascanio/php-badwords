3d3ac2d0548ce0434fab611508bee56e3190b7a9
edoardo.ascanio <edoardoascanio8@gmail.com>
2021-06-05 13:29:14 +0200

    correzioni php

--- index.php
+++ index.php
@@ -2,37 +2,44 @@
 
 $paragrafo = "Dietro il palazzo c'è un povero cane pazzo, chi darà un pezzo di pane a quel povero pazzo cane. Sopra al terrazzo, c'è un cane pazzo, te' pazzo cane, sto pezzo di pane. Dentro quel palazzo c'è un povero cane pazzo, date un pezzo di pane a quel povero pazzo cane.";
 
-$badWord = $GET["badword"];
+$badWord = $GET["badword"] ? $_GET["badword"]: "";
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <p>
-       <?php   
-       
-       echo $testo;
-       echo "<br>";
-       echo strlen($testo);
-       
-       ?>
+        <?php
+
+        echo $testo;
+        echo "<br>";
+        echo strlen($testo);
+
+        ?>
     </p>
 
     <p>
-    
-    <?php   
-     
-     echo str_replace($badword, "***" ,$testo)
-    
-    ?>
+
+        <?php
+
+        $testoModificato = str_replace($badword, "***", $testo);
+
+        echo $testoModificato;
+        echo "<br>";
+        echo strlen($testoModificato);
+
+        ?>
 
     </p>
 </body>
-</html>
+
+</html> 
